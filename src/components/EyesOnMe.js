bcda6e95e38686e50b8c04d669d5c8d231b27e59
Elisabeth Garcia <lisgarciae@gmail.com>
2023-05-26 12:42:27 -0400

    complete

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+function EyesOnMe() {
+    function handleFocus() {
+        return console.log('Good!')
+    }
+    function handleBlur() {
+        return console.log('Hey! Eyes on me!')
+    }
+    return <button 
+        placeholder = 'Eyes on me'
+        onFocus= {(e) => {handleFocus(e)}}
+        onBlur = {(e) => {handleBlur(e)}}
+    >Eyes on me</button>
+
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,20 @@
 // Code Keypad Component Here
+import React from 'react';
 
-function Keypad (){
+function Keypad (e){
+    function handleChange(e) {
+        return console.log('Entering password...')
+    }
     return (
-        <div></div>
+        <div>
+            <input type ="password" 
+                onChange = {handleChange}
+            />
+        </div>
     )
+
 }
+
+
 
 export default Keypad;
